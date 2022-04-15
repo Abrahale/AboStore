c499f51bd750bcfae85a78cd1a2de6119688defb
Ab_Kiros <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>
2022-04-15 15:50:19 +0200

    department and category

--- projects/abo-store-admin/src/app/department/department.component.ts
+++ projects/abo-store-admin/src/app/department/department.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DepartmentService } from '../../services/department.service';
 
 @Component({
   selector: 'app-department',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department.component.scss']
 })
 export class DepartmentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  headers = ["Id","Name", "Description"];
+  departments=[];
+  departmentForm:any;
+  constructor(private departmentService: DepartmentService, private formBuilder:FormBuilder) {
+    this.departmentForm= new FormGroup({
+      name: new FormControl('Abrahale',Validators.required),
+      description: new FormControl('Software developer'),
+    })
   }
+    ngOnInit(): void {
+      this.departmentService.getAllDepartments().subscribe(res => {
+        console.log(res.result)
+        res.result.forEach(e => {
+          this.departments.push({
+            id:e._id,
+            name:e.name,
+            description:e.description,
+          })
+        });
+      })
+    }
+  
+    submitNewCategory(){
+      this.departmentService.AddNewDepartment(this.departmentForm.getRawValue()).subscribe(ab => console.log(ab))
+    }
 
 }
