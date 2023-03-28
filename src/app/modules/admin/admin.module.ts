ab1e1edae027792af8a059b9c44d730cf94386d7
Abrahale Kiros <abrahalekiros@gmail.com>
2023-03-28 10:13:37 +0200

    configure admin route, and imoprting shared mogules

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DepartmentService } from './services/department.service';
 import { CategoryService } from './services/category.service';
+import { RouterModule } from '@angular/router';
+import { adminRoutes } from './admin-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(adminRoutes),
+    SharedModule,
+    ComponentsModule,
   ],
   providers:[
     DepartmentService,
