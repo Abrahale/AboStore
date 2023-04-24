042427f73a0879df0a699b5853c03bbb916f8f70
Abrahale Kiros <abrahalekiros@gmail.com>
2023-04-24 05:27:21 +0200

    edit and reset form

--- src/app/modules/admin/department/department.component.ts
+++ src/app/modules/admin/department/department.component.ts
@@ -13,9 +13,10 @@ import { DepartmentActions, DepartmentSelectors } from 'src/app/store/department
 })
 export class DepartmentComponent implements OnInit{
   department={name:'',description:''}
-
+  departmentForm:NgForm;
   subscription$;
   departments$;
+  isEditMode = false;
   constructor(private depService:DepartmentService, private store$:Store<BaseStoreState.State>){
     this.store$.dispatch(new DepartmentActions.LoadRequestAction());
   }
@@ -23,10 +24,22 @@ export class DepartmentComponent implements OnInit{
     this.departments$ = this.store$.select(DepartmentSelectors.selectData)
   }
   onSubmit() {
-    this.store$.dispatch(new DepartmentActions.AddNewDepartmentAction(this.department))
+    if(!this.isEditMode){
+      this.store$.dispatch(new DepartmentActions.AddNewDepartmentAction(this.department))
+    }
+    else{
+      console.log('Still to be implemented, dispatch action for edit')
+    }
+    this.clearForm()
   }
   editDepartment(input:any){
+    this.isEditMode = true
     this.department.name = input.name
     this.department.description = input.description
+  }
+
+  clearForm():void{
+    this.department = {name:null, description:null}
+    this.isEditMode = false
   }
 }
