565829ca0ab559f3dc08335d6e6255f941057d81
Abrahale Kiros <abrahalekiros@gmail.com>
2023-05-19 12:52:40 +0200

    deleting user capability

--- src/app/store/users/effects.ts
+++ src/app/store/users/effects.ts
@@ -3,12 +3,13 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of as observableOf } from 'rxjs';
 import * as featureActions from './actions';
 import { catchError, map, switchMap } from "rxjs";
-import { UsersService } from "src/app/services/users.service";
+import { UserService } from "src/app/modules/admin/services/user.service";
+
 
 @Injectable()
 export class UsersEffects {
-  constructor(private usersService: UsersService, private actoins$: Actions ){
-  }
+  constructor(private usersService: UserService, private actoins$: Actions ){
+  }s
 
   loadData$ = createEffect(() =>this.actoins$.pipe(
     ofType<featureActions.LoadRequestAction>(
@@ -22,5 +23,30 @@ export class UsersEffects {
     ),
     ),
   ))
+
+  addNewUser$ = createEffect(()=>this.actoins$.pipe(
+    ofType<featureActions.AddNewUserLoadRequest>(
+      featureActions.ActionTypes.ADD_NEW_USER_LOAD_REQUEST
+    ),
+    switchMap(action => this.usersService.AddNewUser(action.payload).pipe(
+      map(data => new featureActions.LoadSuccessAction(data),
+      ),
+      catchError(error => observableOf(new featureActions.LoadFailureAction(error)),
+      ),
+    ),)
+  ),)
+
+
+  deleteUser$ = createEffect(() => this.actoins$.pipe(
+    ofType<featureActions.DeleteUser>(
+      featureActions.ActionTypes.DELET_USER
+    ),
+    switchMap(action => this.usersService.deletUser(action.payload).pipe(
+      map(data => new featureActions.LoadSuccessAction(data),
+      ),
+      catchError(error => observableOf(new featureActions.LoadFailureAction(error))
+      )
+    ))
+  ),)
 
 }
--- src/app/store/users/reducer.ts
+++ src/app/store/users/reducer.ts
@@ -2,13 +2,15 @@ import { initialState, State } from "./state";
 import { Actions, ActionTypes} from "./actions";
 export function UsersReducer(state = initialState, action: Actions): State{
   switch(action.type){
-      case ActionTypes.LOAD_REQUEST : {
+      case ActionTypes.LOAD_REQUEST:  
+      case ActionTypes.ADD_NEW_USER_LOAD_REQUEST : {
         return {
           ...state,
           isLoading: true,
           error: null
         }
       }
+
       case ActionTypes.LOAD_SUCCESS : {
         return {
           ...state,
@@ -17,6 +19,7 @@ export function UsersReducer(state = initialState, action: Actions): State{
           error: null
         }
       }
+
       case ActionTypes.LOAD_FAILURE : {
         return {
           ...state,
@@ -24,11 +27,19 @@ export function UsersReducer(state = initialState, action: Actions): State{
           error: action.payload.error,
         }
       }
+
       case ActionTypes.UPDATE_FORMINPUT : {
         return {
           ...state,
           isLoading: false,
           //formData: action.payload,
+        }
+      }
+
+      case ActionTypes.DELET_USER : {
+        return {
+          ...state,
+          isLoading:true,
         }
       }
 
